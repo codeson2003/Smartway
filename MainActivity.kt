a0e581a70b86e8a49c79ef0f830dd3ba9ecadb04
Carson Stept <steptcarson@gmail.com>
2024-02-17 14:16:04 -0500

    changed the default name argument in fun Greeting()

--- MainActivity.kt
+++ MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Carson")
+                    Greeting("carlos")
                 }
             }
         }
@@ -30,7 +30,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String = "your_name", modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
         modifier = modifier
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     SmartwayTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
