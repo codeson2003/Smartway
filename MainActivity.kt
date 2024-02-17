da66baa7b8835b52c9a9ee8db3dd3b391b77d9c6
Carson Stept <steptcarson@gmail.com>
2024-02-17 13:47:08 -0500

    test 2

--- MainActivity.kt
+++ MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Carlos")
+                    Greeting("Carson")
                 }
             }
         }
